755624c3f930018dc23cf7a2c82ed54a53ed99be
Edgar Munoz <munozedgar510@gmail.com>
2022-07-07 00:58:25 -0700

    got the button to work

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: ""
+    };
+  }
+
+  onChange(e) {
+    let files = e.target.files;
+    console.warn("data file", files)
+
+    let reader = new FileReader();
+    reader.readAsDataURL(files[0]);
+    reader.onload = (e) => { 
+      console.warn("file data ", e.target.result)
+    }
+  }
+  render = () => {
+    return (
+    <div className="App" onSubmit={this.onFormSubmit}>
+      <h1>Resume Uploader</h1>
+      <input type="file" name="file" onChange={(e) => this.onChange(e)}/>
     </div>
-  );
+    )
+  };
 }
 
 export default App;
